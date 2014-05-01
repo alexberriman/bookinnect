1fb7f5d4c4c964fd59806a6ef466cdf38831dd78
Alex Berriman <alexander.berriman@student.uts.edu.au>
2014-04-30 18:13:41 -0700

    Added gitignore file, config file to php5.4 array style

--- server/www/protected/config/main.php
+++ server/www/protected/config/main.php
@@ -1,90 +1,91 @@
 <?php
 
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
+/**
+ * Local configuration file.
+ *
+ * @author Alex Berriman <alex@ajberri.com>
+ */
 
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+return [
+	'basePath' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name' => 'My Web Application',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload' => ['log'],
 
 	// autoloading model and component classes
-	'import'=>array(
+	'import' => [
 		'application.models.*',
 		'application.components.*',
-	),
+	],
 
-	'modules'=>array(
+	'modules' => [
 		// uncomment the following to enable the Gii tool
 		/*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+		'gii' => [
+			'class' => 'system.gii.GiiModule',
+			'password' => 'Enter Your Password Here',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters' => ['127.0.0.1','::1'),
 		),
 		*/
-	),
+	],
 
 	// application components
-	'components'=>array(
-		'user'=>array(
+	'components' => [
+		'user' => [
 			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
+			'allowAutoLogin' => true,
+		],
 		// uncomment the following to enable URLs in path-format
 		/*
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		'urlManager' => [
+			'urlFormat' => 'path',
+			'rules' => [
+				'<controller:\w+>/<id:\d+>' => '<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+				'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		'db' => [
+			'connectionString'  =>  'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		],
 		// uncomment the following to use a MySQL database
 		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
+		'db' => [
+			'connectionString'  =>  'mysql:host=localhost;dbname=testdrive',
+			'emulatePrepare'  =>  true,
+			'username'  =>  'root',
+			'password'  =>  '',
+			'charset'  =>  'utf8',
 		),
 		*/
-		'errorHandler'=>array(
+		'errorHandler' => [
 			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
+			'errorAction' => 'site/error',
+		],
+		'log' => [
+			'class' => 'CLogRouter',
+			'routes' => [
+				[
+					'class' => 'CFileLogRoute',
+					'levels' => 'error, warning',
+				],
 				// uncomment the following to show log messages on web pages
 				/*
-				array(
-					'class'=>'CWebLogRoute',
+				[
+					'class' => 'CWebLogRoute',
 				),
 				*/
-			),
-		),
-	),
+			],
+		],
+	],
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
-	'params'=>array(
+	'params' => [
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
-);
+		'adminEmail' => 'webmaster@example.com',
+	],
+];
